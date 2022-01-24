e76d83c0b1cb8a06e940b5c721961f3f1460c448
dev <arukinagara.kangaeru@gmail.com>
2022-01-24 14:56:21 +0900

    Initial Commit

---
+++ SwiftCamera/SwiftCameraApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftCameraApp.swift
+//  SwiftCamera
+//
+//  Created by dev on 2022/01/24.
+//
+
+import SwiftUI
+
+@main
+struct SwiftCameraApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
